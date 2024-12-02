e95aa6cfd50c5d673c448ccc673b95c2a0bd5b06
MalWalters <malcolm.walters@gmail.com>
2024-12-02 17:29:19 +1100

    Working solution for Day 1 Part 1

--- 2024/Day-01/Day-01-01.py
+++ 2024/Day-01/Day-01-01.py
@@ -3,9 +3,10 @@ import logging
 import logging.config
 import yaml
 
+sourceFileName = 'input.txt'
+
 def setLog(default_path='loggingConfig.yaml', default_level=logging.INFO):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    default_path = os.path.join(dir_path,default_path)
+    default_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), default_path)
     path = default_path
     if os.path.exists(path):
         with open(path, 'rt') as f:
@@ -20,7 +21,40 @@ def setLog(default_path='loggingConfig.yaml', default_level=logging.INFO):
         print('Config file not found')
     return
 
-def main() -> None: 
+def readData(sourceFileName: str) -> list:
+    logger.info("Reading from source file.")
+    sourceFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), sourceFileName)
+    if os.path.exists(sourceFile):
+        with open(sourceFile, 'rt') as f:
+            try:
+                data = f.read().splitlines()
+            except Exception as e:
+                logger.error(e)
+    return data
+
+def calcDiff(data: list) -> int:
+    logger.info('Splitting into two lists')
+    leftCol = []
+    rightCol = []
+    total = 0
+    for line in data:
+        leftCol.append(line.split()[0])
+        rightCol.append(line.split()[1])
+    if len(leftCol) != len(rightCol):
+        logger.warning("List lengths don't match")
+    logger.info('Sorting lists')
+    leftCol.sort()
+    rightCol.sort()
+    lineCount = 0
+    logger.info('Calculating total difference')
+    while lineCount < len(leftCol):
+        total +=  abs(int(leftCol[lineCount]) - int(rightCol[lineCount]))
+        lineCount += 1
+    return total
+
+def main() -> None:
+    data = readData(sourceFileName)
+    logger.info(f'The total difference is {calcDiff(data)}')
     return
 
 if __name__ == '__main__':
--- 2024/README.md
+++ 2024/README.md
@@ -2,4 +2,8 @@
 
 ## Why?
 
-These are the solutions for the 2024 Advent of Code (https://adventofcode.com) challenges
+These are the solutions for the 2024 Advent of Code (https://adventofcode.com) challenges.
+
+Maybe the lists are only off by a small amount! To find out, pair up the numbers and measure how far apart they are. Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
+
+Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances. For example, if you pair up a 3 from the left list with a 7 from the right list, the distance apart is 4; if you pair up a 9 with a 3, the distance apart is 6.
